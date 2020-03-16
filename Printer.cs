f30ae9d60d142989f4fcfc57e4f4970722431d91
muhammad-rifqi-pramudya-hidayat <62230963+muhammad-rifqi-pramudya-hidayat@users.noreply.github.com>
2020-03-16 16:16:22 +0700

    Add files via upload

---
+++ Printer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TheSuperComputer
+{
+	class Printer
+	{
+		private String brand;
+		private String series;
+		private int ppm;
+
+		public Printer(String brand, String series, int ppm)
+		{
+			this.brand = brand;
+			this.series = series;
+			this.ppm = ppm;
+		}
+		public string getBrand()
+		{
+			return this.brand;
+		}
+		public String getSeries()
+		{
+			return this.series;
+		}
+		public int getPpm()
+		{
+			return this.ppm;
+		}
+		public override string ToString()
+		{
+			return $"Printer brand : {this.brand} ; Printer series : {this.series} ; printer paper per minute (ppm) : {this.ppm}";
+
+		}
+	}
+}
